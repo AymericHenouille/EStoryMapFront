ccced6c8b5df6da441ac022e0c50a3435b0b6bb5
AymericHenouille <aymeric.henouille@gmail.com>
2022-03-04 18:13:17 +0100

    feat: workspace connections

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,18 +4,24 @@ import { AuthGuard } from './guards/auth.guard';
 import { ThemesService } from './services/themes.service';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './services/http.service';
+import { WorkspaceService } from './services/workspaces.service';
 
 @NgModule({
   declarations: [
     NavBarComponent
   ],
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [
     AuthGuard,
     ThemesService,
-    AuthService
+    AuthService,
+    HttpService,
+    WorkspaceService
   ]
 })
 export class CoreModule { }
